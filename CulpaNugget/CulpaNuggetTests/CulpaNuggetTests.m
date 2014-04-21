51e846a8c90a441ab5dfbcd4153086ea77e0456f
Don Yu <don@betterment.com>
2014-04-21 17:45:57 -0400

    add in iOS project for culpa nugget

---
+++ CulpaNugget/CulpaNuggetTests/CulpaNuggetTests.m
@@ -0,0 +1,34 @@
+//
+//  CulpaNuggetTests.m
+//  CulpaNuggetTests
+//
+//  Created by Don Yu on 4/16/14.
+//  Copyright (c) 2014 Don Yu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CulpaNuggetTests : XCTestCase
+
+@end
+
+@implementation CulpaNuggetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
